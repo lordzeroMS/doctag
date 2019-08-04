77cfb5d9d6df1c6630ac28b367f0aa56791f8670
Dennis Löhr <dennis_loehr@mckinsey.com>
2019-08-04 16:35:53 +0200

    intent fix and return id for uploads

--- file_upload.php
+++ file_upload.php
@@ -77,10 +77,8 @@ if (move_uploaded_file($_FILES['file']['tmp_name'], $uploadfile)) {
     mysqli_real_escape_string($db, $_FILES['file']['name'])."','".
     $uploadfile.".png','".mysqli_real_escape_string($db, $user)."',
     '".mysqli_real_escape_string($db, $ocr)."', '".mysqli_real_escape_string($db, $ext)."');";
-    $res = selectDb($db, $sql);
-    $last_id = $db->insert_id;
-    print "result:";
-    print json_encode($res);
+    selectDb($db, $sql);
+    $last_id = mysql_insert_id($db);
     close($db, True);
     print "last_id:";
     print $last_id;
